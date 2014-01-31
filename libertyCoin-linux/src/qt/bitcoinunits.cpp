662263807c644dc728e2efb40892d301090d8c27
milan <milandashara@gmail.com>
2014-01-31 14:22:50 +0800

    Change name from litecoin to libertyCoin

--- libertyCoin-linux/src/qt/bitcoinunits.cpp
+++ libertyCoin-linux/src/qt/bitcoinunits.cpp
@@ -45,9 +45,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Litecoins");
-    case mBTC: return QString("Milli-Litecoins (1 / 1,000)");
-    case uBTC: return QString("Micro-Litecoins (1 / 1,000,000)");
+    case BTC: return QString("Libertycoins");
+    case mBTC: return QString("Milli-Libertycoins (1 / 1,000)");
+    case uBTC: return QString("Micro-Libertycoins (1 / 1,000,000)");
     default: return QString("???");
     }
 }
